ae492d799d701f5776e7010bee411a002b37c865
Frederick Morrison <frederick.morrison@outlook.com>
2024-02-07 11:50:34 -0700

    show pitfalls of class variables

--- main.py
+++ main.py
@@ -9,6 +9,24 @@ if __name__ == '__main__':
 
     # create Object
     s1 = Student('Emma', 10)
-    print('Before')
+    print(f's1: {s1}')
     s1.show()
+
+
+    s2 = Student('Vishnu', 11)
+    print(f's2: {s2}')
+    # create a variable that "shadows" the class variable of the same
+    s2.school_name = 'XYZ school'
+    print(f's2: {s2}')
+    print(f's2.school_name: {s2.school_name}')
+    # does s1 have a school_name instance variable?
+    if s1.school_name is None:
+        print('s1 does not have a school_name')
+    else:
+        if s1.school_name == Student.school_name:
+            print(f's1.school_name is identical to Student.school_name: {s1.school_name}')
+        else:
+            print(f's1.school_name is different from Student.school_name: {s1.school_name}')
+
+
 
--- student.py
+++ student.py
@@ -1,13 +1,18 @@
 from typing import ClassVar
+
+
 class Student:
     # Class variable
-    school_name : ClassVar[str]  = 'ABC School '
+    school_name: ClassVar[str] = 'ABC School '
 
     # constructor
-    def __init__(self, name, roll_no):
+    def __init__(self, name: str, roll_no: int):
         self.name = name
         self.roll_no = roll_no
 
     # Instance method
     def show(self):
         print(self.name, self.roll_no, Student.school_name)
+
+    def __repr__(self):
+        return f'name: {self.name}, roll_no: {self.roll_no}, school: {Student.school_name}'
