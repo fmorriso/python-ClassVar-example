8086b27cc57ac7afd39270035b0f3e29645040b8
Frederick Morrison <frederick.morrison@outlook.com>
2024-02-08 11:02:07 -0700

    simplify code to show how shadowing of ClassVar can occur

--- main.py
+++ main.py
@@ -9,30 +9,19 @@ def get_python_version() -> str:
 if __name__ == '__main__':
     print(f"Python version: {get_python_version()}")
 
-    # create Object
+    # create a Student instance
     s1 = Student('Emma', 10)
     print(f's1: {s1}')
-    s1.show()
 
-
+    # Create a second Student
     s2 = Student('Vishnu', 11)
     print(f'before s2: {s2}')
     # WARNING: this will create a student instance variable of type Student
     #          that "shadows" the class variable of the same name and type.
-    s2.__school = School('XYZ_School', '9876 E. Main', 1700)
+    #          This means s2 has BOTH a School class variable AND a School instance variable.
+    s2.school = School('XYZ_School', '9876 E. Main', 1700)
     print(f'after  s2: {s2}')
-    if hasattr(s2,'__school'):
-        print(f'after s2.__school (instance): {s2.__school}')
-    else:
-        print('s2 does not have an instance variable named __school')
-
-    s2.show()
     
     # Now go back and look at Student s1 to see if its School was affected by
     # what we did to Student s2:
     print(f'after  s1: {s1}')
-    if hasattr(s1,'__school'):
-        print(f'after s1.__school: {s1.__school}')
-    else:
-        print('s1 does not have an instance variable named __school')
-    s1.show()
--- student.py
+++ student.py
@@ -4,25 +4,13 @@ from school import School
 
 class Student:
     # Class variable (e.g., equivalent to static variable in Java and C#)
-    __school: ClassVar[str] = School('ABC School', '123 Main Street', 1600)
+    school: ClassVar[School] = School('ABC School', '123 Main Street', 1600)
 
     # constructor
     def __init__(self, name: str, roll_no: int):
         self.name = name
         self.roll_no = roll_no
 
-    # Instance method
-    def show(self):
-        # check for shadow variable the same names as the class variable
-        if self.__school is None:
-            print(self.name, self.roll_no, Student.__school)
-        else:
-            print(self.name, self.roll_no, self.__school)
-
-    def __repr__(self):
-        # check for shadow variable the same names as the class variable
-        if hasattr(self, '__school'):
-            return f'name: {self.name}, roll_no: {self.roll_no}, __school (instance): {self.__school}'
-        else:
-            return f'name: {self.name}, roll_no: {self.roll_no}, __school (ClassVar): {Student.__school}'
+    def __str__(self):
+        return f'name: {self.name}, roll_no: {self.roll_no}, school: {self.school}'
 
